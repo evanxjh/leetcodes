3f50f1d2a99272ddb579956fd8780d75eeb0fb24
evanxjh <xjhsjtu2017@gmail.com>
2019-07-25 16:38:03 +0800

    finished

--- src/two_pointers/SumOfSquareNumbers.java
+++ src/two_pointers/SumOfSquareNumbers.java
@@ -6,6 +6,19 @@ package two_pointers;
  * @author: EvenHsia
  * @create: 2019-07-25 16:26
  **/
+
+/**
+ * Given a non-negative integer c, your task is to decide whether there're two integers a and b such that a2 + b2 = c.
+ *
+ * Example 1:
+ * Input: 5
+ * Output: True
+ * Explanation: 1 * 1 + 2 * 2 = 5
+ *
+ * Example 2:
+ * Input: 3
+ * Output: False
+ */
 public class SumOfSquareNumbers {
     public boolean judgeSquareSum(int c){
         for (int i=0,j=(int) Math.ceil(Math.sqrt(c));i<=j;){
