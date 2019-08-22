123cf6e1c3f83418c1e25de0ce193b0e2cbb431b
evanxjh <xjhsjtu2017@gmail.com>
2019-08-22 17:14:39 +0800

    review:构造背包

--- src/dynamic_programming/TargetSum.java
+++ src/dynamic_programming/TargetSum.java
@@ -37,4 +37,15 @@ public class TargetSum {
         }
         return dp[W];
     }
+
+    //可以用dfs方法
+    public int findTargetSumWays2(int[]nums,int S){
+        return dfs(nums,0,S);
+    }
+    private int dfs(int[] nums,int start,int S){
+        if (start==nums.length){
+            return S==0?1:0;
+        }
+        return dfs(nums,start+1,S-nums[start])+dfs(nums,start+1,S+nums[start]);
+    }
 }
