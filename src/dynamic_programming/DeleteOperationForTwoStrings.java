48c63368cc4c3655b70ee945f2f152fab3dff248
evanxjh <xjhsjtu2017@gmail.com>
2019-08-26 16:09:48 +0800

    Review

--- src/dynamic_programming/DeleteOperationForTwoStrings.java
+++ src/dynamic_programming/DeleteOperationForTwoStrings.java
@@ -16,7 +16,9 @@ package dynamic_programming;
  * Explanation: You need one step to make "sea" to "ea" and another step to make "eat" to "ea".
  */
 
-//由编辑距离想到的，同时对于此类问题，只需要一个一维数组的空间即可。
+//方法一：可以用LCS（最长公共字串），答案为两个字符串长度之和减去LCS的长度*2
+
+//方法二：由编辑距离想到的，同时对于此类问题，只需要一个一维数组的空间即可。
 public class DeleteOperationForTwoStrings {
     public int minDistance(String word1,String word2){
         int length1=word1.length();
@@ -26,7 +28,7 @@ public class DeleteOperationForTwoStrings {
             dp[i]=i;
         }
         for (int i=1;i<=length2;i++){
-            int old=dp[0];
+            int old=dp[0];                              //需要用一个old变量，用来记录原二维数组中的dp[i-1][j-1]项
             for (int j=0;j<=length1;j++){
                 if (j==0){
                     dp[j]+=1;
@@ -34,7 +36,7 @@ public class DeleteOperationForTwoStrings {
                 }
                 int min;
                 if (word1.charAt(j-1)!=word2.charAt(i-1)){
-                    min=Math.min(dp[j],dp[j-1])+1;
+                    min=Math.min(dp[j],dp[j-1])+1;      //不能直接将此值赋给dp[j]，因为再此之前要将其保存给old，以便在下一个j中使用
                 } else {
                     min=old;
                 }
